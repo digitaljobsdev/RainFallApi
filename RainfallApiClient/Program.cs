2baf239b19fdb69158bbf8013050892054d54fdf
Jayson Pascual <>
2024-01-11 07:40:44 +0800

    Test API

--- RainfallApiClient/Program.cs
+++ RainfallApiClient/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main()
+    {
+        string apiUrl = "http://environment.data.gov.uk/flood-monitoring/id/stations/{id}/measures";
+        string stationId = "026090"; // Replace with the actual station ID
+
+        using (HttpClient client = new HttpClient())
+        {
+            try
+            {
+                // Make the GET request
+                HttpResponseMessage response = await client.GetAsync(apiUrl.Replace("{id}", stationId));
+
+                // Check if the request was successful
+                if (response.IsSuccessStatusCode)
+                {
+                    // Read and output the response content
+                    string responseData = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(responseData);
+                }
+                else
+                {
+                    Console.WriteLine($"Error: {response.StatusCode} - {response.ReasonPhrase}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception: {ex.Message}");
+            }
+        }
+    }
+}
